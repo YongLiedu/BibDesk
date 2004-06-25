b4afd27ff7f41041e7385bd4415ba0be71b54873
mmcc <mmcc@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2004-06-25 17:13:14 +0000

    first import of handy string routine

---
+++ bibdesk_vendorsrc/scifihifi/NSString_XMLUtils/NSString+XMLUtils.m
@@ -0,0 +1,27 @@
+/*
+
+NSString+XMLUtils.m
+by Buzz Andersen
+
+More information at: http://www.scifihifi.com/weblog/mac/NSString-Entities.html
+
+This work is licensed under the Creative Commons Attribution License. To view a copy of this license, visit
+
+http://creativecommons.org/licenses/by/1.0/
+
+or send a letter to Creative Commons, 559 Nathan Abbott Way, Stanford,
+California 94305, USA.
+
+*/
+
+#import "NSString+XMLUtils.h"
+
+
+@implementation NSString (XMLUtils)
+
+- (NSString *) stringByUnescapingEntities: (NSDictionary *) entitiesDictionary {
+    NSString *unescapedString = (NSString *) CFXMLCreateStringByUnescapingEntities(NULL, (CFStringRef) self, (CFDictionaryRef) entitiesDictionary);
+    return [unescapedString autorelease];
+}
+
+@end
