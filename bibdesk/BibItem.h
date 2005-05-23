6e703104ab80118568968eb1b6e1f967810cae52
nobody <nobody@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-05-23 21:22:55 +0000

    This commit was manufactured by cvs2svn to create tag 'PRE_FIX_BUG_1207912'.

--- bibdesk/BibItem.h
+++ bibdesk/BibItem.h
@@ -1,8 +1,8 @@
 // BibItem.h
 // Created by Michael McCracken on Tue Dec 18 2001.
 /*
- This software is Copyright (c) 2001,2002, Michael O. McCracken
- All rights reserved.
+ This software is Copyright (c) 2001,2002,2003,2004,2005
+ Michael O. McCracken. All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions
