924a6800f50a129d56930a24b79c145b62465f0e
nobody <nobody@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-06-05 09:23:49 +0000

    This commit was manufactured by cvs2svn to create tag 'POST_BUG_1213603'.

--- bibdesk/BibPref_AutoFile.m
+++ bibdesk/BibPref_AutoFile.m
@@ -1,10 +1,40 @@
 //
 //  BibPref_AutoFile.m
-//  Bibdesk
+//  BibDesk
 //
 //  Created by Michael McCracken on Wed Oct 08 2003.
-//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
-//
+/*
+ This software is Copyright (c) 2003,2004,2005
+ Michael O. McCracken. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+ - Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+ - Neither the name of Michael O. McCracken nor the names of any
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "BibPref_AutoFile.h"
 #import "NSImage+Toolbox.h"
@@ -30,7 +60,7 @@
 		
 		// use a BibItem with some data to build the preview local-url
 		BibItem *tmpBI = [[BibItem alloc] init];
-		[tmpBI setField:BDSKTitleString toValue:@"Bibdesk, a great application to manage your bibliographies"];
+		[tmpBI setField:BDSKTitleString toValue:@"BibDesk, a great application to manage your bibliographies"];
 		[tmpBI setField:BDSKAuthorString toValue:@"McCracken, M. and Maxwell, A. and Howison, J. and Routley, M. and Spiegel, S.  and Porst, S. S. and Hofman, C. M."];
 		[tmpBI setField:BDSKYearString toValue:@"2004"];
 		[tmpBI setField:BDSKMonthString toValue:@"11"];
@@ -114,11 +144,11 @@
 
 - (IBAction)formatHelp:(id)sender{
 	// Panther only
-	//[[NSHelpManager sharedHelpManager] openHelpAnchor:@"citekeyFormat" inBook:@"BibDesk Help"];
+	//[[NSHelpManager sharedHelpManager] openHelpAnchor:@"Autogeneration-Format-Syntax" inBook:@"BibDesk Help"];
 	// ..or we need Carbon/AppleHelp.h
-	OSErr err = AHLookupAnchor((CFStringRef)@"BibDesk Help",(CFStringRef)@"Citation-Key-Format-Reference");
+	OSErr err = AHLookupAnchor((CFStringRef)@"BibDesk Help",(CFStringRef)@"Autogeneration-Format-Syntax");
     if (err == kAHInternalErr || err == kAHInternalErr){
-        NSLog(@"Help Book: error looking up anchor \"Citation-Key-Format-Reference\"");
+        NSLog(@"Help Book: error looking up anchor \"Autogeneration-Format-Syntax\"");
     }
 }
 
