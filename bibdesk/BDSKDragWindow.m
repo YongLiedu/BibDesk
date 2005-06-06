11b3b4a42c739ce792d6d9b1ffa4b8e7cda8e3d4
nobody <nobody@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-06-06 08:57:44 +0000

    This commit was manufactured by cvs2svn to create tag 'POST_RFE_1213519'.

--- bibdesk/BDSKDragWindow.m
+++ bibdesk/BDSKDragWindow.m
@@ -1,15 +1,36 @@
+// BDSKDragWindow.m
 /*
-This software is Copyright (c) 2002, Michael O. McCracken
-All rights reserved.
+ This software is Copyright (c) 2002,2003,2004,2005
+ Michael O. McCracken. All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
 
-- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
--  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
--  Neither the name of Michael O. McCracken nor the names of any contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ - Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+ - Neither the name of Michael O. McCracken nor the names of any
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
 #import "BDSKDragWindow.h"
@@ -29,8 +50,16 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
     pboard = [sender draggingPasteboard];
 
     if ( [[pboard types] containsObject:NSStringPboardType] ) {
+        unsigned modifier = GetCurrentKeyModifiers();
+        if ( (modifier & (optionKey | cmdKey)) == (optionKey | cmdKey) ){ // hack to get the correct cursor
+            return NSDragOperationLink;
+        }
         if (sourceDragMask & NSDragOperationCopy) {
             return NSDragOperationCopy;
+        }
+        // we don't get this when dragging from the document's tableview, since control-leftmouse gives the context menu
+        if (sourceDragMask & NSDragOperationLink){
+            return NSDragOperationLink;
         }
     }
     if ( [[pboard types] containsObject:NSFilenamesPboardType] ) {
@@ -47,7 +76,6 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
     NSData *pbData; 
     NSArray *fileNames;
     NSArray *draggedPubs;
-    NSEnumerator *draggedPubsE;
     BibItem *tempBI;
     NSMutableDictionary *bibDict;
     NSEnumerator *newKeyE;
@@ -106,41 +134,78 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
         }
         [pbString release]; 
         if(hadProblems) return NO;
+        
+        tempBI = [draggedPubs objectAtIndex:0]; // no point in dealing with multiple pubs for a single editor
+
+        // Test a keyboard mask so that we can override all fields when dragging into the editor window (option)
+        // create a crossref (cmd-option), or fill empty fields (no modifiers)
+        // use the Carbon function since [[NSApp currentEvent] modifierFlags] won't work if we're not the front app
+        unsigned modifier = GetCurrentKeyModifiers();
+
+        // we don't seem to get NSDragOperationLink unless it's control-left mouse, so test the mask manually
+        if( (sourceDragMask & NSDragOperationLink) || 
+            (modifier & (optionKey | cmdKey)) == (optionKey | cmdKey) ){
+			
+			NSString *crossref = [tempBI citeKey];
+			NSString *message = nil;
+			
+			// first check if we don't create a Crossref chain
+			if ([[editorBib citeKey] caseInsensitiveCompare:crossref] == NSOrderedSame) {
+				message = NSLocalizedString(@"An item cannot cross reference to itself.", @"");
+			} else {
+				BibDocument *doc = [editorBib document]; 
+				NSString *parentCr = [[doc publicationForCiteKey:crossref] valueOfField:BDSKCrossrefString inherit:NO];
+				
+				if (parentCr && ![parentCr isEqualToString:@""]) {
+					message = NSLocalizedString(@"Cannot cross reference to an item that has the Crossref field set.", @"");
+				} else if ([doc citeKeyIsCrossreffed:[editorBib citeKey]]) {
+					message = NSLocalizedString(@"Cannot set the Crossref field, as the current item is cross referenced.", @"");
+				}
+			}
+			
+			if (message) {
+				NSRunAlertPanel(NSLocalizedString(@"Invalid Crossref Value", @"Invalid Crossref Value"),
+								message,
+								NSLocalizedString(@"OK", @"OK"), nil, nil);
+				return NO;
+			}
+            // add the crossref field if it doesn't exist, then set it to the citekey of the drag source's bibitem
+            if(![[[editorBib pubFields] allKeys] containsObject:BDSKCrossrefString])
+                [editorBib addField:BDSKCrossrefString];
+            [editorBib setField:BDSKCrossrefString toValue:crossref];
+            [[self windowController] setupForm];
+            return YES;
+        }
+        
+        // we aren't linking, so here we decide which fields to overwrite, and just copy values over
+        bibDict = [tempBI pubFields];
+        newKeyE = [bibDict keyEnumerator];
             
-        draggedPubsE = [draggedPubs objectEnumerator];
-        while(tempBI = [draggedPubsE nextObject]){
-            bibDict = [tempBI pubFields];
-            newKeyE = [bibDict keyEnumerator];
+        if(modifier & optionKey){
+            [editorBib setCiteKeyString:[tempBI citeKey]];
+            // just setting the citekey won't update the form, so we have to use a notification
+            [[NSNotificationCenter defaultCenter] postNotificationName:BDSKBibItemChangedNotification object:editorBib userInfo:[NSDictionary dictionaryWithObjectsAndKeys:BDSKCiteKeyString, @"key", [tempBI citeKey], @"value", nil]];
+        }
+        while(key = [newKeyE nextObject]){
+            if(modifier & optionKey){
+                [editorBib setField:key toValue:[bibDict objectForKey:key]];
+            } else {
+                // only set to the new value if the old one is non-existent
+                value = [[editorBib pubFields] objectForKey:key]; // value is the value of key in the dragged-onto window.
+//                NSLog(@"a key is %@, its value is [%@]", key, value);
+                if (([oldKeys containsObject:key] &&
+                     [value isEqualToString:@""]) ||
+                    (![oldKeys containsObject:key] &&
+                     ![[bibDict objectForKey:key] isEqualToString:@""])){
 
-            // Test a keyboard mask so that sometimes we can override all fields when dragging into the editor window
-            // use the Carbon function since [[NSApp currentEvent] modifierFlags] won't work if we're not the front app
-            unsigned modifier = GetCurrentKeyModifiers();
-            if(modifier == optionKey){
-                [editorBib setCiteKeyString:[tempBI citeKey]];
-                // just setting the citekey won't update the form, so we have to use a notification
-                [[NSNotificationCenter defaultCenter] postNotificationName:BDSKBibItemChangedNotification object:editorBib userInfo:[NSDictionary dictionaryWithObjectsAndKeys:BDSKCiteKeyString, @"key", [tempBI citeKey], @"value", nil]];
-            }
-            while(key = [newKeyE nextObject]){
-                if(modifier == optionKey){
-                    [editorBib setField:key toValue:[bibDict objectForKey:key]];
-                } else {
-                    // only set to the new value if the old one is non-existent
-                    value = [[editorBib pubFields] objectForKey:key]; // value is the value of key in the dragged-onto window.
-    //                NSLog(@"a key is %@, its value is [%@]", key, value);
-                    if (([oldKeys containsObject:key] &&
-                         [value isEqualToString:@""]) ||
-                        (![oldKeys containsObject:key] &&
-                         ![[bibDict objectForKey:key] isEqualToString:@""])){
-
-                        [editorBib setField:key
-                                    toValue:[bibDict objectForKey:key]];
-                    }
+                    [editorBib setField:key
+                                toValue:[bibDict objectForKey:key]];
                 }
             }
-            [editorBib setType:[tempBI type]];
-            [[self windowController] updateTypePopup]; // set the popup properly
-            [[self windowController] bibTypeDidChange:nil]; // re-setup the form
-        }//for each dragged-in pub
+        }
+        [editorBib setType:[tempBI type]];
+        [[self windowController] updateTypePopup]; // set the popup properly
+        [[self windowController] bibTypeDidChange:nil]; // re-setup the form
     }
     return YES;
 }
--- bibdesk/BibPref_CiteKey.m
+++ bibdesk/BibPref_CiteKey.m
@@ -3,8 +3,38 @@
 //  
 //
 //  Created by Christiaan Hofman on 11/4/04.
-//  Copyright 2004 __MyCompanyName__. All rights reserved.
-//
+/*
+ This software is Copyright (c) 2004,2005
+ Christiaan Hofman. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+ - Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+ - Neither the name of Christiaan Hofman nor the names of any
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "BibPref_CiteKey.h"
 #import "NSImage+Toolbox.h"
@@ -36,7 +66,7 @@
 		
 		// use a BibItem with some data to build the preview cite key
 		BibItem *tmpBI = [[BibItem alloc] init];
-		[tmpBI setField:BDSKTitleString toValue:@"Bibdesk, a great application to manage your bibliographies"];
+		[tmpBI setField:BDSKTitleString toValue:@"BibDesk, a great application to manage your bibliographies"];
 		[tmpBI setField:BDSKAuthorString toValue:@"McCracken, M. and Maxwell, A. and Howison, J. and Routley, M. and Spiegel, S.  and Porst, S. S. and Hofman, C. M."];
 		[tmpBI setField:BDSKYearString toValue:@"2004"];
 		[tmpBI setField:BDSKMonthString toValue:@"11"];
@@ -93,9 +123,12 @@
 
 - (IBAction)formatHelp:(id)sender{
 	// Panther only
-	//[[NSHelpManager sharedHelpManager] openHelpAnchor:@"citekeyFormat" inBook:@"BibDesk Help"];
+	//[[NSHelpManager sharedHelpManager] openHelpAnchor:@"Autogeneration-Format-Syntax" inBook:@"BibDesk Help"];
 	// ..or we need Carbon/AppleHelp.h
-	AHLookupAnchor((CFStringRef)@"BibDesk Help",(CFStringRef)@"format");
+	OSStatus err = AHLookupAnchor((CFStringRef)@"BibDesk Help",(CFStringRef)@"Autogeneration-Format-Syntax");
+    if (err == kAHInternalErr || err == kAHInternalErr){
+        NSLog(@"Help Book: error looking up anchor \"Autogeneration-Format-Syntax\"");
+    }
 }
 
 - (IBAction)changeCiteKeyAutogenerate:(id)sender{
