36a8d3a914f5b415ded7ea9e04befa540bdd8444
nobody <nobody@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-06-27 17:22:41 +0000

    This commit was manufactured by cvs2svn to create tag 'PRE_BUG_1223405'.

--- bibdesk/BibPref_CiteKey.m
+++ bibdesk/BibPref_CiteKey.m
@@ -3,8 +3,38 @@
 //  
 //
 //  Created by Christiaan Hofman on 11/4/04.
-//  Copyright 2004 __MyCompanyName__. All rights reserved.
-//
+/*
+ This software is Copyright (c) 2004,2005
+ Christiaan Hofman. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+ - Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+ - Neither the name of Christiaan Hofman nor the names of any
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "BibPref_CiteKey.h"
 #import "NSImage+Toolbox.h"
@@ -31,12 +61,14 @@
 	// update the UI elements
     [citeKeyAutogenerateCheckButton setState:[defaults boolForKey:BDSKCiteKeyAutogenerateKey] ? NSOnState : NSOffState];
     [citeKeyLowercaseCheckButton setState:[defaults boolForKey:BDSKCiteKeyLowercaseKey] ? NSOnState : NSOffState];
+    [formatCleanRadio selectCellWithTag:[defaults integerForKey:BDSKCiteKeyCleanOptionKey]];
+	
 	if ([[BDSKFormatParser sharedParser] validateFormat:&citeKeyFormat forField:BDSKCiteKeyString inFileType:BDSKBibtexString error:&error]) {
 		[self setCiteKeyFormatInvalidWarning:NO message:nil];
 		
 		// use a BibItem with some data to build the preview cite key
 		BibItem *tmpBI = [[BibItem alloc] init];
-		[tmpBI setField:BDSKTitleString toValue:@"Bibdesk, a great application to manage your bibliographies"];
+		[tmpBI setField:BDSKTitleString toValue:@"BibDesk, a great application to manage your bibliographies"];
 		[tmpBI setField:BDSKAuthorString toValue:@"McCracken, M. and Maxwell, A. and Howison, J. and Routley, M. and Spiegel, S.  and Porst, S. S. and Hofman, C. M."];
 		[tmpBI setField:BDSKYearString toValue:@"2004"];
 		[tmpBI setField:BDSKMonthString toValue:@"11"];
@@ -93,9 +125,12 @@
 
 - (IBAction)formatHelp:(id)sender{
 	// Panther only
-	//[[NSHelpManager sharedHelpManager] openHelpAnchor:@"citekeyFormat" inBook:@"BibDesk Help"];
+	//[[NSHelpManager sharedHelpManager] openHelpAnchor:@"Autogeneration-Format-Syntax" inBook:@"BibDesk Help"];
 	// ..or we need Carbon/AppleHelp.h
-	AHLookupAnchor((CFStringRef)@"BibDesk Help",(CFStringRef)@"format");
+	OSStatus err = AHLookupAnchor((CFStringRef)@"BibDesk Help",(CFStringRef)@"Autogeneration-Format-Syntax");
+    if (err == kAHInternalErr || err == kAHInternalErr){
+        NSLog(@"Help Book: error looking up anchor \"Autogeneration-Format-Syntax\"");
+    }
 }
 
 - (IBAction)changeCiteKeyAutogenerate:(id)sender{
@@ -106,6 +141,10 @@
 - (IBAction)changeCiteKeyLowercase:(id)sender{
     [defaults setBool:([sender state] == NSOnState) forKey:BDSKCiteKeyLowercaseKey];
 	[self updateUI];
+}
+
+- (IBAction)setFormatCleanOption:(id)sender{
+	[defaults setInteger:[[sender selectedCell] tag] forKey:BDSKCiteKeyCleanOptionKey];
 }
 
 - (IBAction)citeKeyFormatAdd:(id)sender{
--- bibdesk/BibTeXParser.h
+++ bibdesk/BibTeXParser.h
@@ -1,10 +1,40 @@
 //
 //  BibTeXParser.h
-//  Bibdesk
+//  BibDesk
 //
 //  Created by Michael McCracken on Thu Nov 28 2002.
-//  Copyright (c) 2002 __MyCompanyName__. All rights reserved.
-//
+/*
+ This software is Copyright (c) 2002,2003,2004,2005
+ Michael O. McCracken. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+ - Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+ - Neither the name of Michael O. McCracken nor the names of any
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <stdio.h>
 #import <Cocoa/Cocoa.h>
@@ -50,13 +80,23 @@
 
 /*!
     @method     macrosFromBibTeXString:hadProblems:
-    @abstract   Parses a BibTeX string as @string{} declarations, and returns an array of dictionary objects describing each one.
-    @discussion Access the objects with @"mkey" for the macro key and @"mstring" for the string definition.
+    @abstract   Parses a BibTeX string as @string{} declarations, and returns a dictionary of keys and definitions.
+    @discussion 
     @param      aString BibTeX as NSString
     @param      hadProblems (description)
     @result     (description)
 */
-+ (NSArray *)macrosFromBibTeXString:(NSString *)aString hadProblems:(BOOL *)hadProblems;
++ (NSDictionary *)macrosFromBibTeXString:(NSString *)aString hadProblems:(BOOL *)hadProblems;
+
+
+/*!
+    @method     macrosFromBibTeXStyle:
+    @abstract   Returns a dictionary of macro definitions from a BibTeX style file (.bst extension).
+    @discussion The definitions take the form <tt>MACRO {ibmjrd} {"IBM Journal of Research and Development"}</tt>
+    @param      styleContents The contents of the bst file as a string
+    @result     Returns nil if nothing was found or an error occurred.
+*/
++ (NSDictionary *)macrosFromBibTeXStyle:(NSString *)styleContents;
     
 /*!
     @method     stringFromBibTeXValue:error:frontMatter:document:
