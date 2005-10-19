595ceb081e09c5c94e9e70b6807af450e76daa02
mmcc <mmcc@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2005-10-19 03:55:45 +0000

    New class that covers editors and authors and gives them a unique static ID

---
+++ bibdesk_2_test/BDSKContributor.h
@@ -0,0 +1,17 @@
+//
+//  BDSKContributor.h
+//  bd2xtest
+//
+//  Created by Michael McCracken on 7/19/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+static long instanceID = 0;
+
+@interface BDSKContributor : NSManagedObject {
+
+}
+
+@end
