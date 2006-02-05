98b87bcfae09fb2242e151e08fb169b2f817d2d6
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2006-02-05 11:41:15 +0000

    Initial commit of an abstract display controller superclass.

---
+++ bibdesk_2_test/BDSKTableDisplayController.h
@@ -0,0 +1,35 @@
+//
+//  BDSKTableDisplayController.h
+//  bd2xtest
+//
+//  Created by Christiaan Hofman on 1/29/06.
+//  Copyright 2006. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ImageBackgroundBox.h"
+
+
+@interface BDSKTableDisplayController : NSObject {
+    IBOutlet NSView *mainView;
+    IBOutlet NSArrayController *itemsArrayController;
+    IBOutlet NSTableView *itemsTableView;
+    IBOutlet ImageBackgroundBox *selectionDetailsBox;
+    NSDocument *document;
+}
+
+- (NSDocument *)document;
+- (void)setDocument:(NSDocument *)newDocument;
+
+- (NSManagedObjectContext *)managedObjectContext;
+
+- (NSView *)view;
+- (NSString *)viewNibName;
+
+- (NSArrayController *)itemsArrayController;
+- (NSTableView *)itemsTableView;
+
+- (BOOL)writeRowsWithIndexes:(NSIndexSet *)rowIndexes toPasteboard:(NSPasteboard *)pboard forType:(NSString *)type;
+- (BOOL)addRelationshipsFromPasteboard:(NSPasteboard *)pboard forType:(NSString *)type parentRow:(int)row keyPath:(NSString *)keyPath;
+
+@end
