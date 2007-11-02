14ba138cf0cf14cb949431e624c07eb8c75e454c
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2007-11-02 03:46:10 +0000

    remove code specific to my setup

--- bibdesk_vendorsrc/amaxwell/FileView/FileView Program/Controller.m
+++ bibdesk_vendorsrc/amaxwell/FileView/FileView Program/Controller.m
@@ -17,20 +17,6 @@
     self = [super init];
     if (self) {
         _filePaths = [[NSMutableArray alloc] initWithCapacity:100];
-        /*
-        [_filePaths addObject:BASEPATH @"WebOfScienceSearch.g"];
-        [_filePaths addObject:BASEPATH @"Sent Messages (Personal).mbox"];
-        [_filePaths addObject:BASEPATH @"tex_test"];
-        [_filePaths addObject:BASEPATH @"navier.bib"];
-        [_filePaths addObject:BASEPATH @"wos_search.l"];
-        [_filePaths addObject:BASEPATH @"test.rtf"];
-        [_filePaths addObject:@"/Volumes/Local/Users/amaxwell/Doctorate/Pictures and Movies/BWsheetflow.mov"];
-        [_filePaths addObject:@"/Volumes/Local/Users/amaxwell/Doctorate/Pictures and Movies/Glasrud 20mm tube FeOH.tiff"];
-        */
-        //unsigned exceptionMask = (NSLogUncaughtExceptionMask|NSLogUncaughtSystemExceptionMask|NSLogUncaughtRuntimeErrorMask|NSLogTopLevelExceptionMask);
-        //[[NSExceptionHandler defaultExceptionHandler] setExceptionHandlingMask:exceptionMask];
-        //[[NSExceptionHandler defaultExceptionHandler] setDelegate:self];
-
     }
     return self;
 }
@@ -52,32 +38,12 @@
     [arrayController setSortDescriptors:[NSArray arrayWithObject:sort]];    
 }
 
-#define URL(x) \
-[NSURL fileURLWithPath:x]
-
-- (void)testBindings
-{
-    [arrayController addObject:URL(BASEPATH @"WebOfScienceSearch.g")];
-    [arrayController addObject:URL(BASEPATH @"Sent Messages (Personal).mbox")];
-    [arrayController addObject:URL(BASEPATH @"tex_test")];
-    [arrayController addObject:URL(BASEPATH @"navier.bib")];
-    [arrayController addObject:URL(BASEPATH @"wos_search.l")];
-    [arrayController addObject:URL(BASEPATH @"test.rtf")];
-    [arrayController addObject:URL(@"/Volumes/Local/Users/amaxwell/Doctorate/Pictures and Movies/BWsheetflow.mov")];
-    [arrayController addObject:URL(@"/Volumes/Local/Users/amaxwell/Doctorate/Pictures and Movies/Glasrud 20mm tube FeOH.tiff")];
-    [arrayController addObject:URL(@"/Volumes/Local/Users/amaxwell/Doctorate/Articles/3st1d-papanicolaou.pdf")];
-    [arrayController addObject:[NSNull null]];
-
-    //[self performSelector:@selector(selectAndSort) withObject:nil afterDelay:5.0f];
-}
-
 - (void)awakeFromNib
 {
     [_fileView setDelegate:self];
     [_fileView setDataSource:nil];
     [_fileView bind:@"iconURLs" toObject:arrayController withKeyPath:@"arrangedObjects" options:nil];
     [_fileView bind:@"selectionIndexes" toObject:arrayController withKeyPath:@"selectionIndexes" options:nil];
-    [self performSelector:@selector(testBindings) withObject:nil afterDelay:1.0f];
 }
 
 - (void)dealloc
@@ -120,9 +86,7 @@
 
 - (void)fileView:(FileView *)aFileView insertURLs:(NSArray *)absoluteURLs atIndexes:(NSIndexSet *)aSet;
 {
-    //[_filePaths addObjectsFromArray:absoluteURLs];
     [arrayController insertObjects:absoluteURLs atArrangedObjectIndexes:aSet];
-    //[arrayController addObjects:absoluteURLs];
 }
 
 - (BOOL)allowsEditingFileView:(FileView *)aView;
