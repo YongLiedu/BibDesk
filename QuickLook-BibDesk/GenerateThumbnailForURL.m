768f7e7795713d9e7ad6cb88fec7d9330d783628
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2007-11-26 00:25:54 +0000

    Rewrite thumbnail generation to be cleaner and hopefully safer.
    - Refactor and eliminate multiple exit points, so leaks and other
      misbehavior can be eliminated more easily
    - Add some comments and explicit constants so intent is more obvious
    - Draw thumbnails for other UTI types that we support
    - Incorporate background-thread-safe string drawing from
      FileView since NSAttributedString isn't necessarily safe

--- QuickLook-BibDesk/GenerateThumbnailForURL.m
+++ QuickLook-BibDesk/GenerateThumbnailForURL.m
@@ -45,131 +45,244 @@
    This function's job is to create thumbnail for designated file as fast as possible
    ----------------------------------------------------------------------------- */
 
-OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
+// Same size as [[NSPrintInfo sharedPrintInfo] paperSize] on my system
+// NSPrintInfo must not be used in a non-main thread (and it's hellishly slow in some circumstances)
+static const NSSize _paperSize = (NSSize) { 612, 792 };
+
+// page margins 20 pt on all edges
+static const CGFloat _horizontalMargin = 20;
+static const CGFloat _verticalMargin = 20;
+static const NSSize _containerSize = (NSSize) { 572, 762 };
+
+// readable in Cover Flow view, and distinguishable as text in icon view
+static const CGFloat _fontSize = 20;
+
+// returns a string with monospaced font
+static NSAttributedString *createAttributedStringWithContentsOfURLByGuessingEncoding(NSURL *url, bool isBibTeX)
+{
+    NSString *content = [[NSString alloc] initWithContentsOfURL:url encoding:NSUTF8StringEncoding error:NULL];
+    if (nil == content)
+        content = [[NSString alloc] initWithContentsOfURL:url encoding:[NSString defaultCStringEncoding] error:NULL];
+    if (nil == content)
+        content = [[NSString alloc] initWithContentsOfURL:url encoding:NSISOLatin1StringEncoding error:NULL];
+
+    NSMutableAttributedString *attrString = nil;
+    
+    if (content) {
+        NSString *shortString = [content substringToIndex:MIN(7000, [content length])];
+        if (true == isBibTeX)
+            attrString = [[BDSKSyntaxHighlighter highlightedStringWithBibTeXString:shortString] mutableCopy];
+        else
+            attrString = [[NSMutableAttributedString alloc] initWithString:shortString];
+        [content release];
+    }
+    
+    if (attrString) {
+        NSFont *fpFont = [NSFont userFixedPitchFontOfSize:_fontSize];
+        [attrString addAttribute:NSFontAttributeName value:fpFont range:NSMakeRange(0, [attrString length])];
+    }
+    
+    return attrString;
+}
+
+// wash the app icon over a white page background
+static void drawBackgroundAndApplicationIconInCurrentContext()
+{
+    [[NSColor whiteColor] setFill];
+    NSRect pageRect = NSMakeRect(0, 0, _paperSize.width, _paperSize.height);
+    NSRectFillUsingOperation(pageRect, NSCompositeSourceOver);
+    
+    NSString *iconPath = [BDSKGetQLMainBundle() pathForResource:@"FolderPenIcon" ofType:@"icns"];
+    NSImage *appIcon = [[NSImage alloc] initWithContentsOfFile:iconPath];
+    
+    NSRect iconRect = NSZeroRect;
+    // draw the icon smaller than the text container
+    NSSize iconSize = NSMakeSize(_containerSize.width * 0.9, _containerSize.width * 0.9);
+    iconRect.size = iconSize;
+    iconRect.origin.x = (_containerSize.width - iconSize.width) / 2;
+    iconRect.origin.y = (_containerSize.height - iconSize.height) / 2;
+    
+    [appIcon drawInRect:iconRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:0.3];
+    [appIcon release];    
+}
+
+// creates a new NSTextStorage/NSLayoutManager/NSTextContainer system suitable for drawing in a thread
+static NSTextStorage *createTextStorage()
+{
+    NSTextStorage *textStorage = [[NSTextStorage alloc] init];
+    NSLayoutManager *lm = [[NSLayoutManager alloc] init];
+    NSTextContainer *tc = [[NSTextContainer alloc] init];
+    [tc setContainerSize:_containerSize];
+    [lm addTextContainer:tc];
+    // don't let the layout manager use its threaded layout (see header)
+    [lm setBackgroundLayoutEnabled:NO];
+    [textStorage addLayoutManager:lm];
+    // retained by layout manager
+    [tc release];
+    // retained by text storage
+    [lm release];
+    // see header; the CircleView example sets it to NO
+    [lm setUsesScreenFonts:YES];
+
+    return textStorage;
+}
+
+// assumes that the current NSGraphicsContext is the destination
+static void drawAttributedStringInCurrentContext(NSAttributedString *attrString)
+{
+    CGContextRef ctxt = [[NSGraphicsContext currentContext] graphicsPort];
+    
+    NSTextStorage *textStorage = createTextStorage();
+    [textStorage beginEditing];
+    [textStorage setAttributedString:attrString];
+    
+    [textStorage endEditing];
+    NSRect stringRect = NSZeroRect;
+    stringRect.size = _paperSize;
+    
+    CGContextSaveGState(ctxt);
+    
+    CGAffineTransform t1 = CGAffineTransformMakeTranslation(_horizontalMargin, _paperSize.height - _verticalMargin);
+    CGAffineTransform t2 = CGAffineTransformMakeScale(1, -1);
+    CGAffineTransform pageTransform = CGAffineTransformConcat(t2, t1);
+    CGContextConcatCTM(ctxt, pageTransform);
+    
+    // objectAtIndex:0 is safe, since we added these to the text storage (so there's at least one)
+    NSLayoutManager *lm = [[textStorage layoutManagers] objectAtIndex:0];
+    NSTextContainer *tc = [[lm textContainers] objectAtIndex:0];
+    
+    NSRange glyphRange;
+    
+    // we now have a properly flipped graphics context, so force layout and then draw the text
+    glyphRange = [lm glyphRangeForBoundingRect:stringRect inTextContainer:tc];
+    NSRect usedRect = [lm usedRectForTextContainer:tc];
+    
+    // NSRunStorage raises if we try drawing a zero length range (happens if you have an empty text file)
+    if (glyphRange.length > 0) {
+        [lm drawBackgroundForGlyphRange:glyphRange atPoint:usedRect.origin];
+        [lm drawGlyphsForGlyphRange:glyphRange atPoint:usedRect.origin];
+    }        
+    CGContextRestoreGState(ctxt);
+    [textStorage release];    
+}
+
+/*
+ Primary drawing function, once you've created the attributed string.
+ 
+ 1) creates a QL context
+ 2) draws the background and icon
+ 3) draws the text content of the string
+ 4) flushes the context and disposes of it
+ 
+*/
+static void drawIconForThumbnailWithAttributedString(QLThumbnailRequestRef thumbnail, NSAttributedString *attrString)
+{
+    CGContextRef ctxt = QLThumbnailRequestCreateContext(thumbnail, *(CGSize *)&_paperSize, FALSE, NULL);
+    NSGraphicsContext *nsContext = [NSGraphicsContext graphicsContextWithGraphicsPort:ctxt flipped:YES];
+    [NSGraphicsContext saveGraphicsState];
+    [NSGraphicsContext setCurrentContext:nsContext];
+    
+    drawBackgroundAndApplicationIconInCurrentContext();
+    drawAttributedStringInCurrentContext(attrString);
+    
+    QLThumbnailRequestFlushContext(thumbnail, ctxt);
+    CGContextRelease(ctxt);
+    
+    [NSGraphicsContext restoreGraphicsState];    
+}
+
+// draws app icon and text for a thumbnail (with syntax highlighting if isBibTeX is true)
+static bool generateThumbnailForTextFile(QLThumbnailRequestRef thumbnail, CFURLRef url, bool isBibTeX)
+{
+    bool didGenerate = false;
+    NSAutoreleasePool *pool = [NSAutoreleasePool new];
+
+    NSAttributedString *attrString = createAttributedStringWithContentsOfURLByGuessingEncoding((NSURL *)url, isBibTeX);
+    
+    if (attrString) {
+        drawIconForThumbnailWithAttributedString(thumbnail, attrString);
+        [attrString release];
+        didGenerate = true;
+    }
+    [pool release];
+    
+    return didGenerate;
+}
+
+// draws index card thumbnail for a metadata cache file
+static bool generateThumbnailForCacheFile(QLThumbnailRequestRef thumbnail, CFURLRef url, CGSize maxSize)
 {
     NSAutoreleasePool *pool = [NSAutoreleasePool new];
     
+    NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfURL:(NSURL *)url];
+    NSBitmapImageRep *imageRep = nil;
+    if (nil != dictionary)
+        imageRep = [BDSKSpotlightIconController imageRepWithMetadataItem:dictionary];
     
-    // return file icons for tiny sizes; this doesn't seem to be used, though; Finder asks for 108 x 107 icons when I have my desktop icon size set to 48 x 48
-    if (maxSize.height > 32) {
+    if (nil != imageRep) {
+        NSRect drawFrame = NSZeroRect;
+        drawFrame.size = *(NSSize *)&maxSize;
+        NSRect srcRect = NSZeroRect;
+        srcRect.size = [imageRep size];
+        CGFloat ratio = MIN(NSWidth(drawFrame) / srcRect.size.width, NSHeight(drawFrame) / srcRect.size.height);
+        drawFrame.size.width = ratio * srcRect.size.width;
+        drawFrame.size.height = ratio * srcRect.size.height;
         
-        if (UTTypeEqual(contentTypeUTI, CFSTR("net.sourceforge.bibdesk.bdskcache"))) {
-            NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfURL:(NSURL *)url];
-
-            if (nil == dictionary) {
-                // !!! early return
-               [pool release];
-                return fnfErr;
-            }
-               
-            NSBitmapImageRep *imageRep = [BDSKSpotlightIconController imageRepWithMetadataItem:dictionary];
-            if (nil == imageRep) {
-                
-                // !!! early return
-                [pool release];
-                return fnfErr;
-            }
-                       
-            NSRect drawFrame = NSZeroRect;
-            drawFrame.size = *(NSSize *)&maxSize;
-            NSRect srcRect = NSZeroRect;
-            srcRect.size = [imageRep size];
-            CGFloat ratio = MIN(NSWidth(drawFrame) / srcRect.size.width, NSHeight(drawFrame) / srcRect.size.height);
-            drawFrame.size.width = ratio * srcRect.size.width;
-            drawFrame.size.height = ratio * srcRect.size.height;
-            
-            // tried using CoreImage here with a CILanczosScaleTransform filter, but it failed for reasons I never figured out
-            // anyway, we need high-quality interpolation, because Finder does a crappy job of scaling in its icon view
-            
-            CGRect cgrect = *(CGRect *)&drawFrame;
-            CGContextRef ctxt = QLThumbnailRequestCreateContext(thumbnail, cgrect.size, TRUE, NULL);
-            [NSGraphicsContext setCurrentContext:[NSGraphicsContext graphicsContextWithGraphicsPort:ctxt flipped:NO]];
-            CGContextClearRect(ctxt, cgrect);
-            CGContextSetInterpolationQuality(ctxt, kCGInterpolationHigh);
-            [imageRep drawInRect:drawFrame];
-            QLThumbnailRequestFlushContext(thumbnail, ctxt);
-            CGContextRelease(ctxt);
-
-            // !!! early return
-            [pool release];
-            return noErr;
-            
-        } else if (UTTypeEqual(contentTypeUTI, CFSTR("net.sourceforge.bibdesk.bib"))) {
-            
-            NSString *btString = [[NSString alloc] initWithContentsOfURL:(NSURL *)url encoding:NSUTF8StringEncoding error:NULL];
-            if (nil == btString)
-                btString = [[NSString alloc] initWithContentsOfURL:(NSURL *)url encoding:[NSString defaultCStringEncoding] error:NULL];
-            if (nil == btString)
-                btString = [[NSString alloc] initWithContentsOfURL:(NSURL *)url encoding:NSISOLatin1StringEncoding error:NULL];
-            
-            NSMutableAttributedString *attrString = nil;
-            if (btString) {
-                unsigned maxIndex = MIN(7000, [btString length]);
-                attrString = [[BDSKSyntaxHighlighter highlightedStringWithBibTeXString:[btString substringToIndex:maxIndex]] mutableCopy];
-                [btString release];
-            }
-                        
-            if (attrString) {
-                [attrString addAttribute:NSFontAttributeName value:[NSFont userFixedPitchFontOfSize:20.0f] range:NSMakeRange(0, [attrString length])];
-                // same as [[NSPrintInfo sharedPrintInfo] paperSize], but NSPrintInfo must not be used in a non-main thread (and it's hellishly slow in some circumstances), so better to just make an approximate paper size
-                NSSize paperSize = NSMakeSize(612, 792);
-                
-                CGContextRef ctxt = QLThumbnailRequestCreateContext(thumbnail, *(CGSize *)&paperSize, FALSE, NULL);
-                NSGraphicsContext *nsContext = [NSGraphicsContext graphicsContextWithGraphicsPort:ctxt flipped:NO];
-                [NSGraphicsContext saveGraphicsState];
-                [NSGraphicsContext setCurrentContext:nsContext];
-                [[NSColor whiteColor] setFill];
-                NSRect pageRect = NSMakeRect(0, 0, paperSize.width, paperSize.height);
-                NSRectFillUsingOperation(pageRect, NSCompositeSourceOver);
-                
-                NSString *iconPath = [BDSKGetQLMainBundle() pathForResource:@"FolderPenIcon" ofType:@"icns"];
-                NSImage *appIcon = [[NSImage alloc] initWithContentsOfFile:iconPath];
-
-                NSRect iconRect = NSZeroRect;
-                NSSize iconSize = NSMakeSize(paperSize.width * 0.9, paperSize.width * 0.9);
-                iconRect.size = iconSize;
-                iconRect.origin.x = (paperSize.width - iconSize.width) / 2;
-                iconRect.origin.y = (paperSize.height - iconSize.height) / 2;
-                [appIcon drawInRect:iconRect fromRect:NSZeroRect operation:NSCompositeSourceOver fraction:0.3];
-                [appIcon release];
-                
-                [attrString drawInRect:NSInsetRect(pageRect, 20.0f, 20.0f)];
-                QLThumbnailRequestFlushContext(thumbnail, ctxt);
-                CGContextRelease(ctxt);
-                [attrString release];
-                [NSGraphicsContext restoreGraphicsState];
-                
-                // !!! early return
-                [pool release];
-                return noErr;
-            }
-        }
-    }
-    /* fallback case: draw the file icon using Icon Services */
-    
-    FSRef fileRef;
-    OSStatus err;
-    if (CFURLGetFSRef(url, &fileRef))
-        err = noErr;
-    else
-        err = fnfErr;
-    
-    IconRef iconRef;
-    CGRect rect = CGRectZero;
-    CGFloat side = MIN(maxSize.width, maxSize.height);
-    rect.size.width = side;
-    rect.size.height = side;
-    if (noErr == err)
-        err = GetIconRefFromFileInfo(&fileRef, 0, NULL, kFSCatInfoNone, NULL, kIconServicesNormalUsageFlag, &iconRef, NULL);
-    if (noErr == err) {
-        CGContextRef ctxt = QLThumbnailRequestCreateContext(thumbnail, rect.size, TRUE, NULL);
-        err = PlotIconRefInContext(ctxt, &rect, kAlignAbsoluteCenter, kTransformNone, NULL, kPlotIconRefNormalFlags, iconRef);
+        // tried using CoreImage here with a CILanczosScaleTransform filter, but it failed for reasons I never figured out
+        // anyway, we need high-quality interpolation, because Finder does a crappy job of scaling in its icon view
+        
+        CGRect cgrect = *(CGRect *)&drawFrame;
+        CGContextRef ctxt = QLThumbnailRequestCreateContext(thumbnail, cgrect.size, TRUE, NULL);
+        [NSGraphicsContext setCurrentContext:[NSGraphicsContext graphicsContextWithGraphicsPort:ctxt flipped:NO]];
+        CGContextClearRect(ctxt, cgrect);
+        CGContextSetInterpolationQuality(ctxt, kCGInterpolationHigh);
+        [imageRep drawInRect:drawFrame];
+        QLThumbnailRequestFlushContext(thumbnail, ctxt);
         CGContextRelease(ctxt);
-        ReleaseIconRef(iconRef);
     }
     
     [pool release];
+    return true;
+}
+
+OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
+{
+    bool didGenerate = false;
+            
+    if (UTTypeEqual(contentTypeUTI, CFSTR("net.sourceforge.bibdesk.bdskcache"))) {
+        didGenerate = generateThumbnailForCacheFile(thumbnail, url, maxSize);
+    } else if (UTTypeEqual(contentTypeUTI, CFSTR("net.sourceforge.bibdesk.bib"))) {
+        didGenerate = generateThumbnailForTextFile(thumbnail, url, true);
+    } else if (UTTypeConformsTo(contentTypeUTI, kUTTypeText)) {
+        // this handles RIS and other plain text types that we support, sans highlighting
+        didGenerate = generateThumbnailForTextFile(thumbnail, url, false);
+    }
     
+    /* fallback case: draw the file icon using Icon Services */
+    if (false == didGenerate) {
+    
+        FSRef fileRef;
+        OSStatus err;
+        if (CFURLGetFSRef(url, &fileRef))
+            err = noErr;
+        else
+            err = fnfErr;
+        
+        IconRef iconRef;
+        CGRect rect = CGRectZero;
+        CGFloat side = MIN(maxSize.width, maxSize.height);
+        rect.size.width = side;
+        rect.size.height = side;
+        if (noErr == err)
+            err = GetIconRefFromFileInfo(&fileRef, 0, NULL, kFSCatInfoNone, NULL, kIconServicesNormalUsageFlag, &iconRef, NULL);
+        if (noErr == err) {
+            CGContextRef ctxt = QLThumbnailRequestCreateContext(thumbnail, rect.size, TRUE, NULL);
+            err = PlotIconRefInContext(ctxt, &rect, kAlignAbsoluteCenter, kTransformNone, NULL, kPlotIconRefNormalFlags, iconRef);
+            CGContextRelease(ctxt);
+            ReleaseIconRef(iconRef);
+        }
+    }
+        
     return noErr;
 }
 
