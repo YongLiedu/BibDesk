c9046eef3cf99593831b26303225540b23f2c92a
amaxwell <amaxwell@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2007-11-28 18:19:46 +0000

    see if 10_5 is defined

--- bibdesk/BDSKLevelIndicatorCell.m
+++ bibdesk/BDSKLevelIndicatorCell.m
@@ -45,7 +45,7 @@
 - (void)_drawRelevancyWithFrame:(NSRect)cellFrame inView:(NSView *)controlView;
 @end
 
-#if MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_5
+#if !defined(MAC_OS_X_VERSION_10_5) || (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_5)
 typedef NSInteger NSBackgroundStyle;
 @interface NSLevelIndicatorStyle (LeopardOnly)
 - (NSBackgroundStyle)backgroundStyle;
