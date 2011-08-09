0714f756c945301484f9d4ceafd881ee18da0c18
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2011-08-09 20:59:37 +0000

    workaround for WebKit bug in Safari 5.1

--- bibdesk_vendorsrc/amaxwell/FileView/FVWebViewIcon.m
+++ bibdesk_vendorsrc/amaxwell/FileView/FVWebViewIcon.m
@@ -185,7 +185,8 @@ static NSString * const FVWebIconWebViewAvailableNotificationName = @"FVWebIconW
         [_webView stopLoading:nil];
         FVAPIAssert([_webView downloadDelegate] == nil, @"downloadDelegate non-nil");
         FVAPIAssert([_webView UIDelegate] == nil, @"UIDelegate non-nil");
-        [_webView release];
+        // autorelease instead of release to workaround rdar://problem/9923439 (WebKit: crash when loading cancelled due to policy delegate callout)
+        [_webView autorelease];
         // may have frames if the load was cancelled?
         [_redirectedFrames removeAllObjects];
         _numberOfWebViews--;
