ad6f0c86584390c7e327019c04d64f4df0c071a7
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2015-01-02 22:43:02 +0000

    Update copyright dates

--- bibdesk/BibImporter/GetMetadataForFile.m
+++ bibdesk/BibImporter/GetMetadataForFile.m
@@ -2,7 +2,7 @@
 #import <Foundation/Foundation.h>
 //  Created by Adam Maxwell on 09/26/04.
 /*
- This software is Copyright (c) 2004-2014
+ This software is Copyright (c) 2004-2015
  Adam Maxwell. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
