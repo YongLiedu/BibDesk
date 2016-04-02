28484e2e7d16313d57212e8b97f2be3f9af2b55b
hofman <hofman@0fceea05-a30d-0410-8a8b-80ef821fb0a1>
2016-04-02 22:49:13 +0000

    don't disable fileview web view icons by default, for now

--- bibdesk_vendorsrc/amaxwell/FileView/FVWebViewIcon.m
+++ bibdesk_vendorsrc/amaxwell/FileView/FVWebViewIcon.m
@@ -85,7 +85,7 @@ static NSString * const FVWebIconWebViewAvailableNotificationName = @"FVWebIconW
     // default value of 5, with valid range (0, 50)
     // however this seems to lead to a lot of crashes on Mavericks and later, so we disable it there
     if (nil == maxViews) {
-        _maxWebViews = (NSInteger)floor(NSAppKitVersionNumber) > NSAppKitVersionNumber10_8 ? 0 : 5;
+        _maxWebViews = (NSInteger)floor(NSAppKitVersionNumber) == NSAppKitVersionNumber10_9 ? 0 : 5;
     }
     else if ([maxViews integerValue] > 50) {
         FVLog(@"Limiting number of webviews to 50 (FVWebIconMaximumNumberOfWebViews = %@)", maxViews);
